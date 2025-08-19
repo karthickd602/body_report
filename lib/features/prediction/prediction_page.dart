b876d7dbd3d61fad96d544a850b5fcccea5b3e03
Karthick Dinesh A <karthickd602@gmail.com>
2025-08-19 12:04:40 +0530

    update

--- lib/features/prediction/prediction_page.dart
+++ lib/features/prediction/prediction_page.dart
@@ -13,60 +13,11 @@ class PredictionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(PredictionController());
 
-    return Scaffold(
-      appBar: TAppBar(title: "Patient Prediction",),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        controller.generateRandomValues();
-      },backgroundColor: TColors.primary,child: Icon(Icons.repeat,color: Colors.white,),),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-
-            /// Auto Generated Values
-            Obx(() => Column(
-              children: [
-                _buildTile("Blood Pressure", "${controller.bp.value} mmHg"),
-                _buildTile("Heart Rate", "${controller.heartRate.value} bpm"),
-                _buildTile("Sugar Level", "${controller.sugar.value} mg/dL"),
-              ],
-            )),
-
-            const SizedBox(height: 30),
-
-            /// Predict Button
-            SizedBox(
-
-              width: 300,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  controller.predict();
-                  controller.savePrediction();
-                },
-                icon: const Icon(Iconsax.activity),
-                label: const Text("Predict Health"),
-              ),
-            ),
-
-            const SizedBox(height: 30),
-
-            /// Prediction Result
-            Obx(() => Text(
-              controller.status.value,
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: controller.status.value.contains("Normal")
-                    ? Colors.green
-                    : Colors.red,
-              ),
-            )),
-          ],
-        ),
-      ),
-    );
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Prediction Page'),
+    ),
+  );
   }
 
   Widget _buildTile(String title, String value) {
